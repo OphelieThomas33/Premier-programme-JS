65879f84fb88e1c92d44d7a25f90326b7734ec72
OphelieThomas33 <ophelie_thomas@hotmail.com>
2022-11-10 18:50:37 +0100

    Complete functions

---
+++ fonctions/exercice-3/script.js
@@ -0,0 +1,19 @@
+// exercice sur les fonctions récursives
+
+function somme(nombre) {   // 3
+
+    if (nombre == 1) {
+        return 1;
+    }
+
+    return nombre + somme(nombre - 1);  // 2
+
+        // retourner 2 + somme(1) 
+
+            // retourner 1 
+
+    // 3 + 2 + 1  = 6 
+
+}
+
+console.log(somme(3));
