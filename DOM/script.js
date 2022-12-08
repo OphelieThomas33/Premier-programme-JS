8752f84aee353633af0cf87c9eaea186f144948a
Ophélie <ophelie_thomas@hotmail.com>
2022-12-08 20:02:12 +0100

    DOM-2

--- DOM/script.js
+++ DOM/script.js
@@ -1,33 +1,33 @@
-// // Récupérer une balise
-// let header = document.getElementsByTagName('header');
-// let h1 = document.querySelector('header');
+// Récupérer une balise
+let header = document.getElementsByTagName('header');
+let h1 = document.querySelector('header');
 
-// // récupérer un id
-// let logo = document.getElementById('logo');
-// let h1 = document.querySelector('#logo');
+// récupérer un id
+let logo = document.getElementById('logo');
+let h1 = document.querySelector('#logo');
 
-// // récupérer une classe 
-// let container = document.getElementsByClassName('container');
-// let h1 = document.querySelector('.container');
+// récupérer une classe 
+let container = document.getElementsByClassName('container');
+let h1 = document.querySelector('.container');
 
-// // récupérer n'importe quel type d'élément
-// let h1 = document.querySelector('h1');    // utilisable avec une balise, un id ou une classe
-// let p = document.querySelectorAll('p');    // toutes les balises <p>
+// récupérer n'importe quel type d'élément
+let h1 = document.querySelector('h1');    // utilisable avec une balise, un id ou une classe
+let p = document.querySelectorAll('p');    // toutes les balises <p>
 
-// console.log(header);
-// console.log(logo);
-// console.log(container);
-// console.log(h1);
+console.log(header);
+console.log(logo);
+console.log(container);
+console.log(h1);
 
 
-// // Modifier une propriété 
-// let h1 = document.querySelector('h1');
-// h1.textContent = "Hello World !"     // modifier du texte
-// h1.innerHTML = "<div style='font-weight: normal'>Hello World !</div>"   // modifier le style
+// Modifier une propriété 
+let h1 = document.querySelector('h1');
+h1.textContent = "Hello World !"     // modifier du texte
+h1.innerHTML = "<div style='font-weight: normal'>Hello World !</div>"   // modifier le style
 
-// console.log(h1);
+console.log(h1);
 
-// Ajouter des éléments 
+// AJOUTER DES ELEMENTS : 
 // 1ère méthode : Ecrire juste un texte à la suite du contenu
 document.write('test'); 
 
@@ -55,3 +55,11 @@ document.body.insertBefore(helloWorld,
 
 // 4ème méthode : 
 document.querySelector('.container').prepend(helloWorld);   // avant un élément
+
+
+// SUPPRIMER DES ELEMENTS :
+let h1 = document.querySelector('h1');
+h1.remove();
+
+// ou 
+document.querySelector('h1').remove();
